786760eb0fc16a2039201775dea1b5f79114ba88
Iqbaldeep_Singh <Iqbaldeep@gmail.com>
2015-11-24 15:40:33 -0800

    ip change

--- config/config.dev.js
+++ config/config.dev.js
@@ -9,6 +9,6 @@ config.server.port = '3999';
 config.logger={};
 config.logger.loglevel = 'info';
 config.logger.logFile = './trace.log';
-config.mongo.dbUrl = 'mongodb://localhost:27017/chat';
+config.mongo.dbUrl = 'mongodb://159.203.248.23:27017/chat';
 
 module.exports = config;
